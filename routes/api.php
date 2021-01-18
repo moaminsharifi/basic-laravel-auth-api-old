5b29f41fdf559495a0b0a0933acd976392f78e96
Amin Sharifi <bigm00bnd@gmail.com>
2021-01-18 21:45:09 +0330

    basic app inited with tested and route and helper

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [AuthController::class,'login']);
+    Route::post('signup', [AuthController::class,'signup']);
+
+    Route::group([
+        'middleware' => 'auth:sanctum'
+    ], function () {
+        Route::get('logout', [AuthController::class,'logout']);
+        Route::get('user', [AuthController::class,'user']);
+    });
+});
---
+++ tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+    }
+    public function tearDown(): void
+    {
+        Artisan::call('migrate:fresh');
+        parent::tearDown();
+    }
+}
